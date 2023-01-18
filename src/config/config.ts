bb8eba3aad0ca21fb4a527d6e22c1217ba62717d
carlosdiazz <c.diazadriann@gmail.com>
2023-01-18 17:46:51 -0400

    Cambios en arcardia service

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,9 @@
-import * as dotenv from 'dotenv'
-dotenv.config()
+import * as dotenv from "dotenv";
+dotenv.config();
 
-export const PORT_APP = process.env.PORT_APP || 3000
-export const SECRET_JWT_TOKEN = process.env.SECRET_JWT_TOKEN || 'TOKEN-MEGA-SECRETO-ULTRA'
-export const MONGO_URI = process.env.MONGO_URI || ''
-export const URL_API = process.env.URL_API || 'http://localhost:4300'
+export const PORT_APP = process.env.PORT_APP || 3000;
+export const SECRET_JWT_TOKEN =
+  process.env.SECRET_JWT_TOKEN || "TOKEN-MEGA-SECRETO-ULTRA";
+export const MONGO_URI = process.env.MONGO_URI || "";
+export const URL_API = process.env.URL_API || "http://localhost:4300";
+console.log("SABE");
